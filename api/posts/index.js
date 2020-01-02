5eb4250d0025a01cfa68743a432e3155d3a8187f
Dylan <Dylan.french,carroll@gmail.com>
2020-01-02 14:26:28 +0000

    CR

--- api/posts/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import {contacts} from './contacts';
-
-const router = express.Router(); // eslint-disable-line
-router.get('/', (req, res) => {
-  res.send({contacts: contacts});
-});
-
-export default router;
